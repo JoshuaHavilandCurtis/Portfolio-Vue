8ae049fa424a719c476616a381a1244af1310f58
Josh <73693469+JoshuaHavilandCurtis@users.noreply.github.com>
2023-03-08 12:23:25 +0000

    responsive work page

--- src/views/WorkView.vue
+++ src/views/WorkView.vue
@@ -18,23 +18,41 @@ export default {
       </div>
 
       <div class="row gx-5 align-items-center">
-        <div class="col-5">
+        <div class="col-12 col-lg-5">
           <div class="work__image">
             <img src="../assets/img/dianebutterworth-portfolio.jpg">
           </div>
         </div>
 
-        <div class="col-7">
+        <div class="col-12 col-lg-7">
           <div class="work__text">
             <h2>Diane Butterworth</h2>
             <h3>Designed and Built by</h3>
 
             <p>Diane Butterworth Ltd is an office planning, design, and technical consultancy. From design to technical products to construction they can support your digital requirements
             Using the latest technology, We built Diane's website in WordPress (CMS) using Advanced Custom Fields, Sage Theme, Bedrock, PHP and Javascript.</p>
+
+            <a class="btn" href="#">Preview</a>
           </div>
         </div>
       </div>
-      
+
+      <div class="row mt-5">
+        <div class="col-12">
+
+          <ul class="work__tags">
+            <li>HTML5</li>
+            <li>CSS3 (SCSS)</li>
+            <li>Javascript</li>
+            <li>WordPress/ACF</li>
+            <li>PHP</li>
+            <li>Roots (Sage Theme)</li>
+            <li>Bootstrap</li>
+          </ul>
+
+        </div>
+      </div>
+
     </container>
   </div>
 </template>
