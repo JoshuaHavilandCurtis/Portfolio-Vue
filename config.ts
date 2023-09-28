643e9f6867b1bd9c9809f3a6853c762e7161c083
Ben <gardinbe@gmail.com>
2023-09-29 00:56:54 +0100

    change config

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@
  */
 export default {
 	strapiUrl: "https://portfolio-api-yi9v.onrender.com", // the domain where strapi is being hosted. please ensure that public access is allowed to all of the necessary endpoints!
-	enableContentCaching: false, // enable/disable content caching on the site: whether api content be cached to sessionStorage to reduce api calls
-	artificialDelay: false, // enable/disable the artificial delay between api calls to show off animations/transitions
-	artificialDelayDuration: 750 // the artifical delay duration in milliseconds
+	enableContentCaching: true, // enable/disable content caching on the site: whether api content be cached to sessionStorage to reduce api calls
+	artificialDelay: true, // enable/disable the artificial delay between api calls to show off animations/transitions
+	artificialDelayDuration: 1000 // the artifical delay duration in milliseconds
 };
