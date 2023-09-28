35aa38dac24343a47f4c7fc03d7c73097f653dbc
Ben <gardinbe@gmail.com>
2023-09-28 21:44:13 +0100

    update render config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import vue from "@vitejs/plugin-vue";
-import path from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
@@ -7,8 +6,8 @@ export default defineConfig({
 	plugins: [vue()],
 	resolve: {
 		alias: {
-			"~": path.join(".", "/node_modules"),
-			"@": path.join(".", "/src")
+			"~": "./node_modules",
+			"@": "./src"
 		}
 	},
 	css: {
